4fc7f910ff56f3eac18439d4a006241fd7bc2c78
YSLCode <YSLCode@users.noreply.github.com>
2020-12-23 02:40:43 +0330

    removed primary dummy post data

--- semicolon/blog/views.py
+++ semicolon/blog/views.py
@@ -1,55 +1,37 @@
 from django.shortcuts import render
 from .models import Post
-#end of default code
+# end of default code
 # Code: from django.http import HttpResponse
-#The import above is used when we need to return pure html code instead of template.
+# The import above is used when we need to return pure html code instead of template.
 
-#views
-# 'posts' is a list containing dictionaries associated with posts
-# THIS IS DUMMY DATA AND MAIN DATA IS GOING TO BE QUERIED FROM DB
-posts = [
-    {
-        'author': 'SomeOne',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'July 28, 2020'
-    },
-        {
-        'author': 'SomeOne2',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'July 29, 2020'
-    }
-]
+# views
 
 
-#function "home". this function is going to handle traffic from the homepage of the blog.
-#this function is going to take in request argument. we will return what we want
+# function "home". this function is going to handle traffic from the homepage of the blog.
+# this function is going to take in request argument. we will return what we want
 # the user to see.
 def home(request):
 
-    #In order to pass out posts' data to the template we create 'context'
+    # In order to pass out posts' data to the template we create 'context'
     context = {
         'posts': Post.objects.all()
     }
 
     # return HttpResponse('<h1>Welcome to SemiColon home</h1>')
-    #the code above is used for html code but it will be a mess
+    # the code above is used for html code but it will be a mess
     # to write a whole file with it. so we use templates in templates
     # folder and use render() in django.shortcuts module to return them.
-    #the render() function already returns an HttpResponse in the background
+    # the render() function already returns an HttpResponse in the background
     return render(request, 'blog/home.html', context)
-    #the 'blog' directory here is actually the one in the 'templates' directory
-    #Adding 'context' as the third parameter, will allow us to use our data in
+    # the 'blog' directory here is actually the one in the 'templates' directory
+    # Adding 'context' as the third parameter, will allow us to use our data in
     # pur templates
-    
 
 
-#This function will handle the "About" page
+# This function will handle the "About" page
 def about(request):
-    #return HttpResponse('About SemiColon')
-    #The same as the home function happens here
-    return render(request, 'blog/about.html',{'title': 'About'})
-    #The third parameter passed, has to be a dictionary and the 
+    # return HttpResponse('About SemiColon')
+    # The same as the home function happens here
+    return render(request, 'blog/about.html', {'title': 'About'})
+    # The third parameter passed, has to be a dictionary and the
     # keys and values will be passed to the template
-
